ce04917e360e88e06662fdb7b813b2bf2b175525
PieterV86 <76055902+PieterV86@users.noreply.github.com>
2024-11-26 23:03:36 +0100

    Update index.js
    showPledgedBy

--- src/routes/wishlist/index.js
+++ src/routes/wishlist/index.js
@@ -64,8 +64,8 @@ export default function (db) {
         title: _CC.lang('WISHLIST_TITLE', wishlist.username),
         name: wishlist.username,
         items,
-        compiledNotes,
-        sharedInfo: wishlist.doc?.info ?? {}
+        sharedInfo: wishlist.doc?.info ?? {},
+        showPledgedBy: _CC.config.showPledgedBy, // Pass the flag to the template
       })
     } catch (error) {
       req.flash('error', error)
